1b1a9cf9acf3ce3431b0b9052292cc4a8f6d02ae
Wankko Ree <wkr@wkr.moe>
2023-10-16 11:02:38 +0800

    Web: 优化题目 [Php 基础]-[echo]

--- web/php基础/echo/src/index.php
+++ web/php基础/echo/src/index.php
@@ -5,15 +5,13 @@ ob_start();
 require("flag.php");
 ob_get_clean();
 
-echo '
-<form method="post">
-<input type="text" name="code" placeholder="执行一段代码">
-<input type="submit" value="提交">
+$code = isset($_POST['code']) ? trim($_POST['code']) : '';
+echo "
+<form method='post'>
+<input type='text' name='code' placeholder='执行一段代码' value='$code'>
+<input type='submit' value='提交'>
 </form>
-';
-if (isset($_POST['code'])) {
-    $code = trim($_POST['code']);
-    if ($code !== '') {
-        eval($code);
-    }
+";
+if ($code !== '') {
+    eval($code);
 }
