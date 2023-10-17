2f7356452448b4107e7fa70a96205df993fdafc0
Wankko Ree <wkr@wkr.moe>
2023-10-17 16:59:41 +0800

    Web: 新增题目 [SQL 注入]-[报错注入 1]/[报错注入 2]

---
+++ web/sql注入/error1/readme.md
@@ -0,0 +1,9 @@
+# CTF Beginner
+
+## SQL 注入
+
+### 报错注入 1
+
+> 题目来自：[Audi-1 / sqli-labs - Less-5/index.php - e96f217](https://github.com/Audi-1/sqli-labs/blob/e96f21776372c8613a7e565106e62bc01a59355e/Less-5/index.php)
+
+听说有人页面上线了还开着保错，我看看是谁。
---
+++ web/sql注入/error2/src/index.php
@@ -0,0 +1,26 @@
+<?php
+highlight_file(__FILE__);
+error_reporting(0);
+mysqli_report(MYSQLI_REPORT_OFF);
+
+
+$id = isset($_GET['id']) ? trim($_GET['id']) : '';
+if ($id !== '') {
+    $conn = new mysqli("localhost", "root", "", "web", 0, '/var/run/mysqld/mysqld.sock');
+    if ($conn->connect_error) {
+        die("连接失败: " . $conn->connect_error);
+    }
+    $sql="SELECT * FROM users WHERE id=\"$id\" LIMIT 0,1";
+    $result = $conn->query($sql);
+    if ($result) {
+        $row = $result->fetch_array();
+        if ($row) {
+            echo '来啦老弟';
+        } else {
+            echo '没有数据';
+        }
+    } else {
+        echo '出错了: ' . $conn->error;
+    }
+    $conn->close();
+}
