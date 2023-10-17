f305e59cdc05ed68d592907ab7e68c8f17f8e1eb
Wankko Ree <wkr@wkr.moe>
2023-10-17 10:13:45 +0800

    Web: 优化题目 [Php 基础]-[var_dump]

--- web/php基础/echo/src/index.php
+++ web/php基础/echo/src/index.php
@@ -1,12 +1,12 @@
 <?php
 $code = isset($_POST['code']) ? trim($_POST['code']) : '';
 
-echo str_replace(
-    '<span style="color: #FF8000">/*FORM*/</span>',
+echo preg_replace(
+    '/<span style="color: #FF8000">\/\*FORM\*\/(<br \/>)?<\/span>/',
     "<form method='post' style='display: inline-flex;margin-block-end: 0;'>".
-        "<textarea name='code' placeholder='执行一段代码' rows='1'>$code</textarea>".
-        "<input type='submit' value='提交' />".
-    "</form>",
+    "<textarea name='code' placeholder='执行一段代码' rows='1'>$code</textarea>".
+    "<input type='submit' value='提交' />".
+    "</form>$1",
     highlight_file(__FILE__.'.bak', true));
 
 
